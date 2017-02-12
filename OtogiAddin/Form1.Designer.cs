6af533faf92fc527fd1c4adf92e460f56999e724
outwardconnection <outwardconnection@gmail.com>
2017-02-12 12:52:01 +0800

    OtogiAddin 1.0.0 版

---
+++ OtogiAddin/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace OtogiAddin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.startFightButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timesTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.processLabel = new System.Windows.Forms.Label();
+            this.stopTrackButton = new System.Windows.Forms.Button();
+            this.stopFightButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(392, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Genymotion for personal use - Sony Xperia Z - 4.3 - API 18 - 1080x1920 (800x1280," +
+    " 160dpi) - 192.168.172.101";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 9F);
+            this.label1.Location = new System.Drawing.Point(22, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Otogi視窗名稱";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 429F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(573, 30);
+            this.tableLayoutPanel1.TabIndex = 3;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(490, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(493, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "追蹤滑鼠座標";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // startFightButton
+            // 
+            this.startFightButton.Location = new System.Drawing.Point(493, 87);
+            this.startFightButton.Name = "startFightButton";
+            this.startFightButton.Size = new System.Drawing.Size(83, 23);
+            this.startFightButton.TabIndex = 6;
+            this.startFightButton.Text = "開始打怪";
+            this.startFightButton.UseVisualStyleBackColor = true;
+            this.startFightButton.Click += new System.EventHandler(this.startFightButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "還剩";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timesTextBox
+            // 
+            this.timesTextBox.Location = new System.Drawing.Point(55, 56);
+            this.timesTextBox.Name = "timesTextBox";
+            this.timesTextBox.Size = new System.Drawing.Size(100, 25);
+            this.timesTextBox.TabIndex = 8;
+            this.timesTextBox.Text = "0";
+            this.timesTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.timesTextBox.TextChanged += new System.EventHandler(this.timesTextBox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(161, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "次";
+            // 
+            // processLabel
+            // 
+            this.processLabel.AutoSize = true;
+            this.processLabel.Location = new System.Drawing.Point(15, 122);
+            this.processLabel.Name = "processLabel";
+            this.processLabel.Size = new System.Drawing.Size(67, 15);
+            this.processLabel.TabIndex = 10;
+            this.processLabel.Text = "現在進程";
+            // 
+            // stopTrackButton
+            // 
+            this.stopTrackButton.Location = new System.Drawing.Point(493, 58);
+            this.stopTrackButton.Name = "stopTrackButton";
+            this.stopTrackButton.Size = new System.Drawing.Size(83, 23);
+            this.stopTrackButton.TabIndex = 11;
+            this.stopTrackButton.Text = "停止追蹤";
+            this.stopTrackButton.UseVisualStyleBackColor = true;
+            this.stopTrackButton.Click += new System.EventHandler(this.stopTrackButton_Click);
+            // 
+            // stopFightButton
+            // 
+            this.stopFightButton.Location = new System.Drawing.Point(493, 87);
+            this.stopFightButton.Name = "stopFightButton";
+            this.stopFightButton.Size = new System.Drawing.Size(83, 23);
+            this.stopFightButton.TabIndex = 12;
+            this.stopFightButton.Text = "停止打怪";
+            this.stopFightButton.UseVisualStyleBackColor = true;
+            this.stopFightButton.Click += new System.EventHandler(this.stopFightButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 172);
+            this.Controls.Add(this.stopFightButton);
+            this.Controls.Add(this.stopTrackButton);
+            this.Controls.Add(this.processLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.timesTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.startFightButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "OtogiAddin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button startFightButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox timesTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label processLabel;
+        private System.Windows.Forms.Button stopTrackButton;
+        private System.Windows.Forms.Button stopFightButton;
+    }
+}
+
---
+++ OtogiAddin/Form1.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Threading;
+
+
+namespace OtogiAddin
+{
+    public partial class Form1 : Form
+    {
+        //Genymotion視窗名稱 "Genymotion for personal use - Sony Xperia Z - 4.3 - API 18 - 1080x1920 (800x1280, 160dpi) - 192.168.172.101"
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam); //用來模擬滑鼠動作
+        public static uint WM_LBUTTONDOWN = 0x201; //0x201
+        public static uint WM_LBUTTONUP = 0x202;   //0x202
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", CharSet = CharSet.Auto)]  
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);  //以視窗名稱獲得窗口句柄
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForgegroundWindow(); //獲得當前視窗窗口句柄
+
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int cmdShow);
+        private const int WS_SHOWNORMAL = 1;
+
+        [DllImport("USER32.DLL")]//引用User32.dll
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect); //獲得窗口四角座標
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left; //最左坐标
+            public int Top; //最上坐标
+            public int Right; //最右坐标
+            public int Bottom; //最下坐标
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(Point Point);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+
+            OtogiWindow = FindWindow(null,textBox1.Text);
+            if(OtogiWindow.ToInt32() != 0)
+            {
+                findOtogi();
+            }
+            else
+            {
+                MessageBox.Show("找不到Otogi視窗");
+            }
+            
+        }
+
+        public void findOtogi()
+        {
+            ShowWindowAsync(OtogiWindow, WS_SHOWNORMAL);
+            SetForegroundWindow(OtogiWindow);
+            GetWindowRect(OtogiWindow, ref OtogiWindowRect);
+            OtogiWindowPoint.X = OtogiWindowRect.Left;  //或得Otogi最左上角座標
+            OtogiWindowPoint.Y = OtogiWindowRect.Top;
+            label2.Text = "(" + OtogiWindowPoint.X.ToString() + "," + OtogiWindowPoint.Y.ToString() + ")";
+        }
+
+        public void play2_3_123()
+        {
+            
+            Point mousePoint = new Point(0, 0);
+            mousePoint = setMousePoint(260, 624);
+            SetCursorPos(mousePoint.X, mousePoint.Y);
+            setProcess("進入2.3.1");
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            for (int i=0;i<300;i+=10)
+            {
+                SetCursorPos(mousePoint.X, mousePoint.Y - i);
+                if(!mySleep(10) || stopFight)
+                {
+                    return;
+                }
+            }
+            if (!mySleep(10) || stopFight)
+            {
+                return;
+            }
+            mouse_event(MOUSEEVENTF_LEFTUP, 0,0, 0, 0);
+            if(!mySleep(1000) || stopFight)
+            {
+                return;
+            }
+
+            Fight(260, 562, 60);
+            if (!mySleep(3000) || stopFight)
+            {
+                return;
+            }
+
+            setProcess("進入2.3.2");
+            mousePoint = setMousePoint(260, 624);
+            SetCursorPos(mousePoint.X, mousePoint.Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            if (!mySleep(100) || stopFight)
+            {
+                return;
+            }
+            if (!mySleep(500) || stopFight)
+            {
+                return;
+            }
+            for (int i = 0; i < 300; i += 10)
+            {
+                SetCursorPos(mousePoint.X, mousePoint.Y - i);
+                if (!mySleep(10) || stopFight)
+                {
+                    return;
+                }
+            }
+            if (!mySleep(10) || stopFight)
+            {
+                return;
+            }
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            if (!mySleep(1000 ) || stopFight)
+            {
+                return;
+            }
+
+            Fight(283, 371);
+            if (!mySleep(3000) || stopFight)
+            {
+                return;
+            }
+
+            setProcess("進入2.3.1");
+            Fight(286, 624);
+            if (!mySleep(3000) || stopFight)
+            {
+                return;
+            }
+        }
+
+        public void Fight(int level_X =286, int level_Y = 624, int battleTime = 60)
+        {
+            if (!mySleep(1000) || stopFight)
+            {
+                return;
+            }
+            findOtogi();
+
+            Point mousePoint = new Point(0, 0);
+            //點擊進入關卡
+            setProcess("進入關卡");
+            mousePoint = setMousePoint(level_X, level_Y);
+            SetCursorPos(mousePoint.X, mousePoint.Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0,0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            if (!mySleep(2000) || stopFight)
+            {
+                return;
+            }
+
+            //確定隊伍
+            setProcess("確定隊伍");
+            mousePoint = setMousePoint(445, 658);
+            SetCursorPos(mousePoint.X, mousePoint.Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            if (!mySleep(2000) || stopFight)
+            {
+                return;
+            }
+
+            //選擇助戰 
+            setProcess("選擇助戰");
+            mousePoint = setMousePoint(302, 587);//(公會幫助1)
+            SetCursorPos(mousePoint.X, mousePoint.Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            if (!mySleep(2000) || stopFight)
+            {
+                return;
+            }
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            if (!mySleep(2000) || stopFight)
+            {
+                return;
+            }
+
+            //點選快進
+            setProcess("點選快進");
+            mousePoint = setMousePoint(462, 59);
+            SetCursorPos(mousePoint.X, mousePoint.Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+            setProcess("等待戰鬥結束...");
+            for(int i=0;i<battleTime*1000;i+=10)
+            {
+                Thread.Sleep(10);
+                if(programClosed || stopFight)
+                {
+                    return;
+                }
+            }
+            //等待戰鬥結束
+
+            //點選快進
+            setProcess("點選快進");
+            mousePoint = setMousePoint(462, 59);
+            SetCursorPos(mousePoint.X, mousePoint.Y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            if (!mySleep(2000) || stopFight)
+            {
+                return;
+            }
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            setProcess("完成關卡");
+            return;
+        }
+
+        private void setProcess(string state)
+        {
+            processLabel.Text = state;
+            return;
+        }
+
+        private bool mySleep(int ms)
+        {
+
+            if (ms > 10)
+            {
+                int times = ms / 10;
+                ms %= 10;
+                for (int i = 0; i < times; i++)
+                {
+                    Thread.Sleep(10);
+                    Application.DoEvents();
+
+                    if (programClosed)
+                    {
+                        return false;
+                    }
+                }
+                Thread.Sleep(ms);
+            }
+            else
+            {
+                Thread.Sleep(ms);
+                Application.DoEvents();
+                if (programClosed)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public Point setMousePoint(int x,int y)
+        {
+            findOtogi();
+            x += OtogiWindowPoint.X;
+            y += OtogiWindowPoint.Y;
+            Point ret = new Point(x, y);
+            return ret;
+        }
+
+        private void getWindownName()
+        {
+            // 關鍵： 先隱藏本表單以利偵側本 Winform 下的程式
+            this.Hide();
+            // 透過 WindowFromPoint 方法來取得當下座標程式的 IntPtr (句柄)
+            Point ptCursor = new Point();
+            IntPtr hWnd = WindowFromPoint(ptCursor);
+            // 取得視窗名稱長度
+            int length = GetWindowTextLength(hWnd) + 1;
+            StringBuilder sb = new StringBuilder(length);
+            // 取得視窗名稱
+            GetWindowText(hWnd, sb, length);
+            textBox1.Text = ""; //清空舊資訊
+            textBox1.Text = sb.ToString();
+            // 最後再把原本的 Winform Show 出來~
+            this.Show();
+
+            return;
+        }
+
+        private void getMousePoint()
+        {
+            stopTrackMouse = false;
+            Point mousePoint = new Point();     
+
+            while (true)
+            {
+                mousePoint.X = Cursor.Position.X;
+                mousePoint.Y = Cursor.Position.Y;
+                label2.Text = "(" + (mousePoint.X-OtogiWindowPoint.X).ToString() + "," + (mousePoint.Y - OtogiWindowPoint.Y).ToString() + ")";
+                //label2.Text = "(" + (mousePoint.X).ToString() + "," + (mousePoint.Y).ToString() + ")";
+                if (!mySleep(10) || stopTrackMouse)
+                {
+                    return;
+                }
+            }
+            
+
+        }
+
+        private static IntPtr OtogiWindow = new IntPtr();
+        private static RECT OtogiWindowRect = new RECT();
+        private static Point OtogiWindowPoint = new Point();
+        private bool stopTrackMouse { set; get; }
+        private static bool programClosed { set; get; }
+        private static bool stopFight { set; get; }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopTrackMouse = true;
+            programClosed = false;
+            stopFight = true;
+            stopTrackButton.Hide();
+            stopFightButton.Hide();
+        }
+
+        private void Form1_Closing(object sender, EventArgs e)
+        {
+            programClosed = true;
+            Console.WriteLine("bye bye");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getMousePoint();
+            stopTrackButton.Show();
+        }
+
+        private void startFightButton_Click(object sender, EventArgs e)
+        {
+            mySleep(1000);
+            int times = Int32.Parse(timesTextBox.Text);
+            if(OtogiWindow.ToInt32() == 0)
+            {
+                setProcess("沒有找到Otogi");
+                return;
+            }
+            if(times<=0)
+            {
+                setProcess("戰鬥次數為0");
+                return;
+            }
+            stopFightButton.Show();
+            times--;
+            timesTextBox.Text = times.ToString();
+            stopFight = false;
+            play2_3_123();
+            setProcess("開始2.3.1 2.3.2 2.3.3副本");
+            if(times>0)
+            {
+                startFightButton_Click(sender,e);
+            }
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopTrackButton_Click(object sender, EventArgs e)
+        {
+            stopTrackMouse = true;
+            stopTrackButton.Hide();
+        }
+
+        private void timesTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopFightButton_Click(object sender, EventArgs e)
+        {
+            stopFight = true;
+            stopFightButton.Hide();
+        }
+    }
+    
+}
